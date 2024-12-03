9e60efd3550d91273725caa771cb26699d429011
= <=>
2024-12-03 09:23:59 +0500

    cookie-cutter-django-practice

---
+++ khalid/khalid/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from khalid.users.models import User
+from khalid.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
